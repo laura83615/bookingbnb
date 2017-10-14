21b035f8ea6f03dec7cbdc803b9c2fd62741f218
蔡淳如 <Laura@caichunrude-MacBook-Pro.local>
2017-10-14 15:31:21 +0800

    add User model

--- bookingbnb/bnb/models.py
+++ bookingbnb/bnb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    account = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    mail = models.EmailField()
+    isadmin = models.BooleanField(default=False)
