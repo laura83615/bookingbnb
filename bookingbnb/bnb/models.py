ec4a52ee76759af9f6eb6240b40177c8a223af7b
蔡淳如 <Laura@caichunrude-MacBook-Pro.local>
2017-10-16 13:37:36 +0800

    model string representation

--- bookingbnb/bnb/models.py
+++ bookingbnb/bnb/models.py
@@ -9,3 +9,6 @@ class User(models.Model):
     name = models.CharField(max_length=20)
     mail = models.EmailField()
     isadmin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return ("account: " + self.account)
